c4dd6cc469fa9e8512f53f49a6d80406504e8c1d
Maria-Gul28 <maria_gul28@yahoo.com>
2025-05-11 20:04:40 +0500

    Implemented login API endpoint

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,54 +1,61 @@
 const express = require('express');
-const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
-
+dotenv.config();
 const logger = pino();  // Create a Pino logger instance
 
+// Create JWT secret
 dotenv.config();
-
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/register', async (req, res) => {
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
     try {
-        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+        // Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
         const db = await connectToDatabase();
-
-        // Task 2: Access MongoDB collection
         const collection = db.collection("users");
 
-        //Task 3: Check for existing email
-        const existingEmail = await collection.findOne({ email: req.body.email });
+        // Check for user credentials in the database
+        const theUser = await collection.findOne({ email: req.body.email });
+        
+        // Check if the user exists
+        if (theUser) {
+            // Check if the password matches the encrypted password
+            let result = await bcryptjs.compare(req.body.password, theUser.password);
+            
+            if (!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong password' });
+            }
+            
+            // Fetch user details from the database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            
+            // Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
 
-        const salt = await bcryptjs.genSalt(10);
-        const hash = await bcryptjs.hash(req.body.password, salt);
-        const email = req.body.email;
-
-        //Task 4: Save user details in database
-        const newUser = await collection.insertOne({
-            email: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: hash,
-            createdAt: new Date(),
-        });
-
-        const payload = {
-            user: {
-                id: newUser.insertedId,
-            },
-        };
-
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User registered successfully');
-        res.json({authtoken,email});
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged in successfully');
+            
+            // Send the response with the JWT token and user details
+            return res.status(200).json({ authtoken, userName, userEmail });
+        } else {
+            // Send message if user not found
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
     } catch (e) {
-         return res.status(500).send('Internal server error');
+        logger.error(e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
     }
 });
 
